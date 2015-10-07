55ed557709844d0eba42b2162215cbee4fbe0de1
Sriram Ramachandran <sriramspb@gmail.com>
2015-10-07 12:31:28 +0530

    Modified modules

--- SSWebApplication/SSWebApplication/QRCode.aspx.designer.cs
+++ SSWebApplication/SSWebApplication/QRCode.aspx.designer.cs
@@ -40,12 +40,12 @@ namespace SSWebApplication {
         protected global::System.Web.UI.WebControls.Button btnGenerate;
         
         /// <summary>
-        /// plBarCode control.
+        /// img control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder plBarCode;
+        protected global::System.Web.UI.WebControls.Image img;
     }
 }
